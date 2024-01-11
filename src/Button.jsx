9a79597c61fbdc8fee9f9e232f71b9f48e1d1831
Abhilash Singh <abhilash4597@gmail.com>
2024-01-12 03:00:32 +0530

    css update

--- src/Button.jsx
+++ src/Button.jsx
@@ -13,20 +13,22 @@ export default function Button({
   ...rest
 })  {
 
-  const classes = classNames( 'flex items-center gap-x-1 px-3 py-1.5 border',{
-    'border-blue-500 bg-blue-500 text-white' : primary,
-    'border-gray-900 bg-gray-900 text-white' : secondary,
-    'border-green-500 bg-green-500 text-white' : success,
-    'border-red-500 bg-red-500 text-white' : danger,
-    'border-yellow-500 bg-yellow-500 text-white' : warning,
-    'rounded-full' : rounded,
-    'bg-white' : outline,
-    'text-blue-500' : outline && primary,
-    'text-gray-900' : outline && secondary,
-    'text-green-500' : outline && success,
-    'text-red-500' : outline && danger,
-    'text-yellow-500' : outline && warning,
-  })
+  const classes = classNames( ...rest.className,
+    'flex items-center gap-x-1 px-3 py-1.5 border',
+    {
+      'border-blue-500 bg-blue-500 text-white' : primary,
+      'border-gray-900 bg-gray-900 text-white' : secondary,
+      'border-green-500 bg-green-500 text-white' : success,
+      'border-red-500 bg-red-500 text-white' : danger,
+      'border-yellow-500 bg-yellow-500 text-white' : warning,
+      'rounded-full' : rounded,
+      'bg-white' : outline,
+      'text-blue-500' : outline && primary,
+      'text-gray-900' : outline && secondary,
+      'text-green-500' : outline && success,
+      'text-red-500' : outline && danger,
+      'text-yellow-500' : outline && warning,
+    })
       return (
         <button {...rest} className= {classes}> {children} </button>
       )
