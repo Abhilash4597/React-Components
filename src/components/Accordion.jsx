79bf42a31c8e07b15783ee7a9d670b5505546b9b
Abhilash Singh <abhilash4597@gmail.com>
2024-01-13 01:17:00 +0530

    update

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,6 +1,16 @@
+/* eslint-disable react/prop-types */
 
 export default function Accordion( {items} ) {
+
+  let renderedItems = items.map((item)=>{
+    return (
+    <div key={item.id}>
+      <div>{item.label}</div>
+      <div>{item.content}</div>
+    </div>
+    )
+  })
   return (
-    <div>{items}</div>
+    <div>{renderedItems}</div>
   )
 }
