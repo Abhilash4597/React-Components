5ac0a9d01aa277c1f4357149a9bc99b1b2dac9f5
Abhilash Singh <abhilash4597@gmail.com>
2024-01-13 02:04:31 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,17 @@ function App() {
 
   const items = [
   {
+    id:'dxbncb',
     label:'Can I use React on a Project',
     content:'Yes you can use React in a project.Yes you can use React in a project.Yes you can use React in a project.Yes you can use React in a project.',
   },
   {
+    id:'jdsjfj',
     label:'Can I use JavaScript on a Project',
     content:'Yes you can use React in a project.Yes you can use React in a project.Yes you can use React in a project.Yes you can use React in a project.',
   },
   {
+    id:'jhfnmn',
     label:'Can I use CSS on a Project',
     content:'Yes you can use React in a project.Yes you can use React in a project.Yes you can use React in a project.Yes you can use React in a project.',
   }]
