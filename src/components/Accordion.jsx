11854c662e1ce5633792f28a96ad407d333b2c67
Abhilash Singh <abhilash4597@gmail.com>
2024-01-14 02:54:27 +0530

    update

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -10,11 +10,11 @@ export default function Accordion( {items} ) {
   const renderedItems = items.map((item,index)=>{
     const isExpanded = expandedIndex === index;
 
-    const icon = <span>{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>;
+    const icon = <span className="text-2xl">{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>;
 
     return (
     <div key={item.id}>
-      <div className="flex p-3 bg-gray-50 border-b items-center cursor-pointer" onClick={()=>{setExpandedIndex(index)}}>
+      <div className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer" onClick={()=>{setExpandedIndex(index)}}>
       {item.label}{icon}
       </div>
       {isExpanded && <div className="border-b p-5">{item.content}</div>}
