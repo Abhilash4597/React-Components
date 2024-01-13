4e264d5642effddb93f9f9cb1468e9ad4c7ec7fb
Abhilash Singh <abhilash4597@gmail.com>
2024-01-14 02:46:48 +0530

    update

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 
-import { useState } from "react"
+import { useState } from "react";
+import { GoChevronDown , GoChevronLeft } from "react-icons/go";
 
 export default function Accordion( {items} ) {
 
@@ -9,9 +10,13 @@ export default function Accordion( {items} ) {
   const renderedItems = items.map((item,index)=>{
     const isExpanded = expandedIndex === index;
 
+    const icon = <span>{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>;
+
     return (
     <div key={item.id}>
-      <div onClick={()=>{setExpandedIndex(index)}}>{item.label}</div>
+      <div onClick={()=>{setExpandedIndex(index)}}>
+      {item.label}{icon}
+      </div>
       {isExpanded && <div>{item.content}</div>}
     </div>
     )
