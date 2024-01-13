74bc126c7c3857f7f1d8476f13815487de9dbfdc
Abhilash Singh <abhilash4597@gmail.com>
2024-01-14 02:29:23 +0530

    update

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -4,14 +4,14 @@ import { useState } from "react"
 
 export default function Accordion( {items} ) {
 
-  const [expandedIndex,setExpandedIndex] = useState(0);
+  const [expandedIndex,setExpandedIndex] = useState(null);
 
   const renderedItems = items.map((item,index)=>{
     const isExpanded = expandedIndex === index;
 
     return (
     <div key={item.id}>
-      <div>{item.label}</div>
+      <div onClick={()=>{setExpandedIndex(index)}}>{item.label}</div>
       {isExpanded && <div>{item.content}</div>}
     </div>
     )
