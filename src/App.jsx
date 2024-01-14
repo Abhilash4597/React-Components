228f4e79428095e531eee5cf5ba7f19f12fb9ee0
Abhilash Singh <abhilash4597@gmail.com>
2024-01-15 01:04:08 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import DropDown from "./components/DropDown"
+
 
 function App() {
-    <div></div>
+    
+  const options = [
+    {label:'Red' , value: 'red'},
+    {label:'Green' , value: 'green'},
+    {label:'Blue' , value: 'blue'},
+  ]
+
+  return (
+    <div>
+      <DropDown options={options} />
+    </div>
+    )
 }
 
 export default App
