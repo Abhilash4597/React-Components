8f95bfef235d83158c7268b3f659e57a4ec863d7
Abhilash Singh <abhilash4597@gmail.com>
2024-01-15 00:56:22 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,6 @@
-import Accordion from "./components/Accordion"
-
 
 function App() {
-
-  const items = [
-  {
-    id:'dxbncb',
-    label:'Can I use React on a Project',
-    content:'Yes you can use React in a project.Yes you can use React in a project.Yes you can use React in a project.Yes you can use React in a project. Yes you can use React in a project.Yes you can use React in a project.Yes you can use React in a project.Yes you can use React in a project. Yes you can use React in a project.Yes you can use React in a project.Yes you can use React in a project.Yes you can use React in a project. Yes you can use React in a project.Yes you can use React in a project.Yes you can use React in a project.Yes you can use React in a project.',
-  },
-  {
-    id:'jdsjfj',
-    label:'Can I use JavaScript on a Project',
-    content:'Yes you can use JavaScript in a project.Yes you can use JavaScript in a project.Yes you can use JavaScript in a project.Yes you can use JavaScript in a project.Yes you can use JavaScript in a project.Yes you can use JavaScript in a project.Yes you can use JavaScript in a project.Yes you can use JavaScript in a project. Yes you can use JavaScript in a project.Yes you can use JavaScript in a project.Yes you can use JavaScript in a project.Yes you can use JavaScript in a project.Yes you can use JavaScript in a project.Yes you can use JavaScript in a project.Yes you can use JavaScript in a project.',
-  },
-  {
-    id:'jhfnmn',
-    label:'Can I use CSS on a Project',
-    content:'Yes you can use CSS in a project.Yes you can use CSS in a project.Yes you can use CSS in a project.Yes you can use CSS in a project. Yes you can use CSS in a project.Yes you can use CSS in a project.Yes you can use CSS in a project.Yes you can use CSS in a project. Yes you can use CSS in a project.Yes you can use CSS in a project.Yes you can use CSS in a project.Yes you can use CSS in a project. Yes you can use CSS in a project.Yes you can use CSS in a project.Yes you can use CSS in a project.Yes you can use CSS in a project.',
-  }]
-
-  return (
-    <>
-      <Accordion items={items} ></Accordion>
-    </>
-  )
+    <div></div>
 }
 
 export default App
