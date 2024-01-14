d3d5069a2dd170fdbdad54c6da928ffe55db1cb8
Abhilash Singh <abhilash4597@gmail.com>
2024-01-15 02:36:30 +0530

    update

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react"
+import { GoChevronDown } from "react-icons/go";
 
 function DropDown( {options , onChange , value} ) {
     const [isOpen , setIsOpen] = useState(false);
@@ -15,7 +16,7 @@ function DropDown( {options , onChange , value} ) {
 
     const renderedOptions = options.map((option)=>{
         return (
-            <div onClick={()=>{handleClickedOption(option)}} key={option.value}>
+            <div className="hover:bg-sky-100 rounded cursor-pointer p-1" onClick={()=>{handleClickedOption(option)}} key={option.value}>
                 {option.label}
             </div>
         )
@@ -27,9 +28,12 @@ function DropDown( {options , onChange , value} ) {
     }
 
   return (
-    <div>
-        <div onClick={handleDropDown}> {content} </div>
-        {isOpen && <div>{renderedOptions}</div>}
+    <div className="w-48 relative">
+        <div className="flex justify-between item-center cursor-pointer border rounded p-3 shadow bg-white w-full" onClick={handleDropDown}>
+            {content}
+            <GoChevronDown className="text-lg mt-1" />
+        </div>
+        {isOpen && <div className="absolute top-full border rounded p-3 shadow bg-white w-full">{renderedOptions}</div>}
     </div>
   )
 }
