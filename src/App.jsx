6688a481f9d7b33f4de7e43b364e0dc6a15b4200
Abhilash Singh <abhilash4597@gmail.com>
2024-01-19 01:38:08 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
-import { useState } from "react"
-import DropDown from "./components/DropDown"
-
 
 function App() {
 
-  const [selection,SetSelection] = useState('');
-
-  const handleSelection= (option)=>{
-    SetSelection(option)
-  }
-    
-  const options = [
-    {label:'Red' , value:'red'},
-    {label:'Green' , value:'green'},
-    {label:'Blue' , value:'blue'},
-  ]
-
   return (
     <div>
-      <DropDown options={options} value={selection} onChange={handleSelection} />
+
     </div>
     )
 }
---
+++ src/pages/DropdownPage.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+import DropDown from "../components/DropDown"
+
+
+function DropdownPage() {
+
+  const [selection,SetSelection] = useState('');
+
+  const handleSelection= (option)=>{
+    SetSelection(option)
+  }
+    
+  const options = [
+    {label:'Red' , value:'red'},
+    {label:'Green' , value:'green'},
+    {label:'Blue' , value:'blue'},
+  ]
+
+  return (
+    <div>
+      <DropDown options={options} value={selection} onChange={handleSelection} />
+    </div>
+    )
+}
+
+export default DropdownPage
